52ce940298dfd6f7a716550641588c81bb8c2988
Luffer <lautaro.cajal@gmail.com>
2023-01-04 22:44:10 -0300

    first commit of public info

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: "/GYM-FITNESS",
   plugins: [
     "gatsby-plugin-postcss",
     `gatsby-plugin-image`,
